ee533baac5d81c4580d4bc44b9f329a47392dab9
Emma Marshall <e.marshall.111@proton.me>
2026-08-04 14:50:40 -0700

    rename write config

--- src/facts_experiment_builder/application/setup_experiment.py
+++ src/facts_experiment_builder/application/setup_experiment.py
@@ -16,8 +16,8 @@ from facts_experiment_builder.core.experiment.experiment_skeleton import (
 from facts_experiment_builder.core.experiment.paths import (
     ExperimentName,
 )
-from facts_experiment_builder.io.write_experiment_metadata import (
-    write_metadata_yaml_jinja2,
+from facts_experiment_builder.io.write_config import (
+    write_config_jinja2,
 )
 from facts_experiment_builder.application.experiment_helpers import (
     experiment_skeleton_to_facts_experiment,
@@ -174,7 +174,7 @@ def finalize_experiment_setup(
     See Also
     --------
     experiment_skeleton_to_facts_experiment : Builds the concrete experiment object.
-    write_metadata_yaml_jinja2 : Renders the configuration file from a template.
+    write_config_jinja2 : Renders the configuration file from a template.
 
     Notes
     -----
@@ -223,15 +223,12 @@ def finalize_experiment_setup(
         projection_scale=projection_scale,
         schemas=schemas,
     )
-    # Write metadata file using templtae
-    metadata_path = experiment_paths.config_path
+    # Write config file using templtae
+    config_path = experiment_paths.config_path
     experiment_config = facts_experiment_to_config(
         experiment_obj=experiment_obj,
-        config_path=metadata_path,
         module_registry_version=version,
     )
 
-    write_metadata_yaml_jinja2(
-        experiment_config=experiment_config, 
-    )
-    return metadata_path
+    write_config_jinja2(experiment_config=experiment_config, config_path=config_path)
+    return config_path
