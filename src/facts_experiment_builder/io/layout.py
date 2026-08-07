6e5d15ff0c3c25938f504e825b7a1a0fad9862e1
Emma Marshall <e.marshall.111@proton.me>
2026-08-07 16:28:04 -0700

    move paths to io

---
+++ src/facts_experiment_builder/io/layout.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+from facts_experiment_builder.core.experiment.name import ExperimentName
+
+_CONFIG_FILENAME = "experiment-config.yaml"
+_COMPOSE_FILENAME = "experiment-compose.yaml"
+_OUTPUT_DIRNAME = "output"
+
+
+@dataclass
+class ExperimentPaths:
+    """
+    This is a class to hold all paths related to an experiment including:
+    - Experiment directory
+    - Experiment parent directory (if exists)
+    - Experiment output directory
+    - Experiment config file
+    - Experiment compose file
+    """
+
+    workspace_dir: Path
+    experiment_name: ExperimentName
+
+    def __post_init__(self) -> None:
+        if not self.workspace_dir.is_absolute():
+            raise ValueError(f"'{self.workspace_dir}.is_absolute()' must be True")
+
+    @property
+    def experiment_dir(self) -> Path:
+        return self.workspace_dir / self.experiment_name.relative_path
+
+    @property
+    def parent_dir(self) -> Path:
+        return self.experiment_dir.parent
+
+    @property
+    def output_dir(self) -> Path:
+        return self.experiment_dir / _OUTPUT_DIRNAME
+
+    @property
+    def config_path(self) -> Path:
+        return self.experiment_dir / _CONFIG_FILENAME
+
+    @property
+    def compose_path(self) -> Path:
+        return self.experiment_dir / _COMPOSE_FILENAME
